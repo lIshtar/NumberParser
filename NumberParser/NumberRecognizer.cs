170f30258a9a174f6383840b8a167c0baeb1927b
DESKTOP-7JMLMMA\Ishtar <daniil020612@gmail.com>
2023-09-09 09:14:04 +0300

    Добавьте файлы проекта.

---
+++ NumberParser/NumberRecognizer.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace NumberParser
+{
+    public class NumberRecognizer
+    {
+        public int GetNumberFromString(string numberText)
+        {
+            numberText = numberText.Trim().ToLower();
+            int number = 0;
+            int hundreds = CountHundreds(ref numberText);
+            if (hundreds == 0)
+            {
+                return 0;
+            }
+
+            number += hundreds;
+            if (numberText.Length > 0)
+            {
+                var tens = CountTens(ref numberText);
+                if (tens == 0)
+                {
+                    return 0;
+                }
+                number += tens;
+                if (tens > 20)
+                {
+                    if (!(numberText == "" || numberText == "s" && tens == 80))
+                    {
+                        int foundUnit = 0;
+                        if (tens == 60 || tens == 80)
+                        {
+                            foundUnit = CountUnitsToTwenty(ref numberText, (SixtyEightyContext)tens);
+                        }
+                        else
+                        {
+                            foundUnit = CountUnitsToTen(ref numberText);
+                        }
+                        if (foundUnit == 0 )
+                        {
+                            return 0;
+                        }
+                        else
+                        {
+                            number += foundUnit;
+                        }
+                    }
+                    else if(numberText == "s" && tens == 80)
+                    {
+                        numberText = "";
+                    }
+                }
+            }
+            if (numberText == "")
+                return number;
+            else
+                return 0;
+        }
+
+        int CountHundreds(ref string numberText)
+        {
+            int number = 0;
+            int centIndex = numberText.IndexOf(OneHundred.Key);
+
+            if (centIndex == -1)
+            {
+                return 0;
+            }
+            else if (centIndex == 0)
+            {
+                number += OneHundred.Value;
+            }
+            else
+            {
+                foreach (var unit in new List<string>(units.Keys))
+                {
+                    if (numberText.IndexOf(unit) == 0 && unit != "un")
+                    {
+                        number += OneHundred.Value * units[unit];
+                        numberText = numberText.Remove(0, unit.Length);
+
+                        numberText = numberText.Trim();
+                        centIndex = numberText.IndexOf(OneHundred.Key);
+                        if (centIndex != 0)
+                        {
+                            return 0;
+                        }
+                        break;
+                    }
+                }
+            }
+            numberText = numberText.Remove(0, OneHundred.Key.Length);
+            numberText = numberText.Trim();
+
+            return number;
+        }
+
+        int CountTens(ref string numberText)
+        {
+            bool isFound = false;
+            int number = 0;
+            foreach(var tenNumber in Tens)
+            {
+                if (numberText.IndexOf(tenNumber.Key) == 0)
+                {
+                    number = tenNumber.Value;
+                    numberText = numberText.Remove(0, tenNumber.Key.Length);
+                    isFound = true;
+                    break;
+                }
+            }
+            if (!isFound)
+            {
+                foreach (var unit in units)
+                {
+                    if (numberText.IndexOf(unit.Key) == 0)
+                    {
+                        number = unit.Value;
+                        numberText = numberText.Remove(0, unit.Key.Length);
+                        isFound = true;
+                        break;
+                    }
+                }
+            }
+            if (!isFound)
+            {
+                foreach (var unit in TenToTwenty)
+                {
+                    if (numberText.IndexOf(unit.Key) == 0)
+                    {
+                        number = unit.Value;
+                        numberText = numberText.Remove(0, unit.Key.Length);
+                        break;
+                    }
+                }
+            }
+            numberText = numberText.Trim();
+            return number;
+        }
+
+        int CountUnitsToTen(ref string numberText)
+        {
+            int number = 0;
+            numberText = numberText.Trim();
+            if (numberText.IndexOf("et") == 0)
+            {
+                numberText = numberText.Remove(0, "et".Length);
+                numberText = numberText.Trim();
+                if (numberText == "un")
+                {
+                    number = 1;
+                    numberText = numberText.Remove(0, "un".Length);
+                }
+            }
+            else
+            {
+                foreach (var unit in units)
+                {
+                    if (numberText.IndexOf(unit.Key) == 0 || numberText.IndexOf($"-{unit.Key}") == 0)
+                    {
+                        if (unit.Key == "un")
+                            return 0;
+                        number = unit.Value;
+                        int deleteLength = unit.Key.Length;
+                        if (numberText.StartsWith('-'))
+                            deleteLength++;
+                        numberText = numberText.Remove(0, deleteLength);
+                        break;
+                    }
+                }
+            }
+            numberText = numberText.Trim();
+            return number;
+        }
+
+        int CountUnitsToTwenty(ref string numberText, SixtyEightyContext OneWordContext)
+        {
+            bool isFound = false;
+            int number = 0;
+            numberText = numberText.Trim();
+            if (numberText.IndexOf("et") == 0 && OneWordContext == SixtyEightyContext.Sixty)
+            {
+                numberText = numberText.Remove(0, "et".Length);
+                numberText = numberText.Trim();
+                if (numberText == "un")
+                {
+                    number = 1;
+                    numberText = numberText.Remove(0, "un".Length);
+                }
+                else if (numberText == "onze")
+                {
+                    number = 11;
+                    numberText = numberText.Remove(0, "onze".Length);
+                }
+            }
+            else
+            {
+                foreach (var unit in units)
+                {
+                    if (numberText.IndexOf(unit.Key) == 0 || numberText.IndexOf($"-{unit.Key}") == 0)
+                    {
+                        if (unit.Key == "un" && OneWordContext == SixtyEightyContext.Sixty)
+                            return 0;
+                        number = unit.Value;
+                        int deleteLength = unit.Key.Length;
+                        if (numberText.StartsWith('-'))
+                            deleteLength++;
+                        numberText = numberText.Remove(0, deleteLength);
+                        isFound = true;
+                        break;
+                    }
+                }
+                if (!isFound)
+                {
+                    foreach (var unit in TenToTwenty)
+                    {
+                        if (numberText.IndexOf(unit.Key) == 0 || numberText.IndexOf($"-{unit.Key}") == 0)
+                        {
+                            if (unit.Key == "onze" && OneWordContext == SixtyEightyContext.Sixty)
+                                return 0;
+                            number = unit.Value;
+                            int deleteLength = unit.Key.Length;
+                            if (numberText.StartsWith('-'))
+                                deleteLength++;
+                            numberText = numberText.Remove(0, deleteLength);
+                            break;
+                        }
+                    }
+                }
+            }
+            return number;
+        }
+
+        enum SixtyEightyContext
+        {
+            Sixty = 60,
+            Eighty = 80
+        }
+
+        KeyValuePair<string, int> OneHundred = new KeyValuePair<string, int>("cent", 100);
+
+        Dictionary<string, int> units = new Dictionary<string, int>()
+        {
+            { "un", 1},
+            { "deux", 2 },
+            { "trois", 3 },
+            { "quatre", 4 },
+            { "cinq", 5 },
+            { "six", 6 },
+            { "sept", 7},
+            { "huit", 8},
+            { "neuf", 9}
+        };
+
+        Dictionary<string, int> Tens = new Dictionary<string, int>()
+        {
+            { "vingt", 20 },
+            { "trente", 30 },
+            { "quarante", 40 },
+            { "cinquante", 50 },
+            { "soixante", 60 },
+            //{ "soixante-dix", 70},
+            { "quatre-vingt", 80},
+            //{ "quatre-vingt-dix", 90}
+        };
+
+
+        //
+        Dictionary<string, int> TenToTwenty = new Dictionary<string, int>()
+        {
+            
+            { "onze", 11 },
+            { "douze", 12 },
+            { "treize", 13 },
+            { "quatorze", 14 },
+            { "quinze", 15 },
+            { "seize", 16 },
+            { "dix-sept", 17},
+            { "dix-huit", 18},
+            { "dix-neuf", 19},
+            { "dix", 10 }
+        };
+    }
+}
---
+++ NumberParserTests/NumberRecognizerTests.cs
@@ -0,0 +1,137 @@
+﻿using NumberParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberParserTests
+{
+    public class NumberRecognizerTests
+    {
+        [Theory]
+        [MemberData(nameof(SeveralCents))]
+        void GetNumberFromString_OnlyHundreds_Passed(string recognizedString, int actualNumber)
+        {
+            //Arrange
+            var numberRecognizer = new NumberRecognizer();
+
+            //Act
+            int recognizedNumber = numberRecognizer.GetNumberFromString(recognizedString);
+
+            //Assert
+            Assert.Equal(actualNumber, recognizedNumber);
+        }
+
+        public static IEnumerable<object[]> SeveralCents()
+        {
+            yield return new object[] { "cent", 100 };
+            yield return new object[] { "Cinq cent", 500 };
+            yield return new object[] { "Six cent", 600 };
+            yield return new object[] { "Neuf cent", 900 };
+            yield return new object[] { "Huit cent", 800 };
+        }
+
+        [Theory]
+        [MemberData(nameof(SeveralNumbersFrom101To159))]
+        void GetNumberFromString_NumberFrom101To159_Passed(string recognizedString, int actualNumber)
+        {
+            //Arrange
+            var numberRecognizer = new NumberRecognizer();
+
+            //Act
+            int recognizedNumber = numberRecognizer.GetNumberFromString(recognizedString);
+
+            //Assert
+            Assert.Equal(actualNumber, recognizedNumber);
+        }
+        public static IEnumerable<object[]> SeveralNumbersFrom101To159()
+        {
+            yield return new object[] { "cent Trente", 130 };
+            yield return new object[] { " cent Vingt", 120 };
+            yield return new object[] { " cent Douze", 112 };
+            yield return new object[] { " cent Sept", 107 };
+            yield return new object[] { " cent Quatre", 104 };
+        }
+
+        [Theory]
+        [MemberData(nameof(NumbersFrom160To199))]
+        void GetNumberFromString_NumberFrom160To199_Passed(string recognizedString, int actualNumber)
+        {
+            //Arrange
+            var numberRecognizer = new NumberRecognizer();
+
+            //Act
+            int recognizedNumber = numberRecognizer.GetNumberFromString(recognizedString);
+
+            //Assert
+            Assert.Equal(actualNumber, recognizedNumber);
+        }
+        public static IEnumerable<object[]> NumbersFrom160To199()
+        {
+            yield return new object[] { "cent Quatre-vingt-dix-neuf ", 199 };
+            yield return new object[] { " cent Quatre-vingt-dix-huit", 198 };
+            yield return new object[] { " cent Quatre-vingt-dix-sept", 197 };
+            yield return new object[] { " cent Quatre-vingt-seize  ", 196 };
+            yield return new object[] { " cent Quatre-vingt-quinze", 195 };
+            yield return new object[] { "cent Quatre-vingt-quatorze ", 194 };
+            yield return new object[] { " cent Quatre-vingt-treize ", 193 };
+            yield return new object[] { " cent Quatre-vingt-douze", 192 };
+            yield return new object[] { " cent Quatre-vingt-onze  ", 191 };
+            yield return new object[] { " cent Quatre-vingt-dix", 190 };
+            yield return new object[] { "cent Quatre-vingt-neuf ", 189 };
+            yield return new object[] { " cent Quatre-vingt-huit", 188 };
+            yield return new object[] { " cent Quatre-vingt-sept", 187 };
+            yield return new object[] { " cent Quatre-vingt-six ", 186 };
+            yield return new object[] { " cent Quatre-vingt-cinq ", 185 };
+            yield return new object[] { "cent Quatre-vingt-quatre", 184 };
+            yield return new object[] { " cent Quatre-vingt-trois", 183 };
+            yield return new object[] { " cent Quatre-vingt-deux ", 182 };
+            yield return new object[] { " cent Quatre-vingt-un  ", 181 };
+            yield return new object[] { " cent Quatre-vingts", 180 };
+            yield return new object[] { "cent Soixante-dix-neuf ", 179 };
+            yield return new object[] { " cent Soixante-dix-huit", 178 };
+            yield return new object[] { " cent Soixante-dix-sept", 177 };
+            yield return new object[] { " cent Soixante-seize ", 176 };
+            yield return new object[] { " cent Soixante-quinze", 175 };
+            yield return new object[] { "cent Soixante-quatorze  ", 174 };
+            yield return new object[] { " cent Soixante-treize", 173 };
+            yield return new object[] { " cent Soixante-douze", 172 };
+            yield return new object[] { " cent Soixante et onze ", 171 };
+            yield return new object[] { " cent Soixante-dix", 170 };
+            yield return new object[] { "cent Soixante-neuf  ", 169 };
+            yield return new object[] { " cent Soixante-huit", 168 };
+            yield return new object[] { " cent Soixante-sept", 167 };
+            yield return new object[] { " cent Soixante-six ", 166 };
+            yield return new object[] { " cent Soixante-cinq ", 165 };
+            yield return new object[] { "cent Soixante-quatre ", 164 };
+            yield return new object[] { " cent Soixante-trois", 163 };
+            yield return new object[] { " cent Soixante-deux", 162 };
+            yield return new object[] { " cent Soixante et un ", 161 };
+            yield return new object[] { " cent Soixante ", 160 };
+        }
+
+        [Theory]
+        [MemberData(nameof(IncorrectSeveralCents))]
+        void GetNumberFromString_IncorrectSeveralCentsString_Zero(string recognizedString)
+        {
+            //Arrange
+            var numberRecognizer = new NumberRecognizer();
+
+            //Act
+            int recognizedNumber = numberRecognizer.GetNumberFromString(recognizedString);
+            int zero = 0;
+
+            //Assert
+            Assert.Equal(zero, recognizedNumber);
+        }
+        public static IEnumerable<object[]> IncorrectSeveralCents()
+        {
+            yield return new object[] { "cents" };
+            yield return new object[] { "Cinq cent q" };
+            yield return new object[] { "Six cent dexa" };
+            yield return new object[] { "Neuf a cent" };
+            yield return new object[] { "a Huit cent" };
+        }
+    }
+}
